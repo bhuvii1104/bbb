2ebfb592538c55b97c7e09f6133cc228d80be95d
bhuvii1104 <bhuvitaj30@gmail.com>
2016-07-10 15:12:22 +0530

    finish

---
+++ rrrrrrr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+if(a>b)
+{
+printf("the given number is positive");
+}
+else
+{
+printf("the given number is negative");
+}
+return0;
+}
